13e8020d780a93502a48fcba5d696048011a3cca
AbdugofforCoder <abdugofforisroilov15@gmail.com>
2025-01-03 17:18:09 +0500

    ff

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,34 @@
 <template>
   <div class="home">
-      bu home page
+    <main>
+      <section class="banner">
+        <div class="container mx-auto">
+          <div class="banner__left ">
+              <div style="width: 400px !important; background-color: red" class="next flex justify-between gap-56">
+                <div class="imdesc flex gap-2 grow">
+                  <img src="/src/assets/icons/Frame 455510.svg" alt="">
+                  <strong class="text-indigo-950">Комплектующие ПК</strong>
+                </div>
+                <img src="/src/assets/icons/Right.svg" alt="">
+              </div>
+              <hr>
+              <div class="next flex gap-56 max-w-96 w-full">
+                <div class="imdesc flex gap-2">
+                  <img src="/src/assets/icons/Monitor.svg" alt="">
+                  <strong class="text-indigo-950">Мониторы</strong>
+                </div>
+                  <img src="/src/assets/icons/Right.svg" alt="">
+              </div>
+          </div>
+          <div class="banner__right"></div>
+        </div>
+      </section>
+    </main>
   </div>
 </template>
 
 <script setup>
-
 </script>
 
 <style lang="scss" scoped>
-
 </style>
