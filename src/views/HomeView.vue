afcdebef8ede012834d97d7a80cee867d09e1e8a
AbdugofforCoder <abdugofforisroilov15@gmail.com>
2025-01-06 15:53:03 +0500

    jonatyapman

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,12 +17,23 @@
           </div>
 
           <!-- Banner Content (Right Side) -->
-          <div class="banner__right flex-1">
-            <!-- O'ng kontent qo'shish uchun joy -->
-          </div>
+          <div class="relative bg-gray-900 text-white">
+  <img src="../assets/images/banner.png" alt="Banner Image" class="w-full object-cover h-72 md:h-96">
+  <div class="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-start p-8">
+    <h1 class="text-4xl md:text-6xl font-bold">TEXT BANNER</h1>
+    <p class="text-lg md:text-xl mt-2">BANNER DESCRIPTION SMALL TEXT</p>
+    <button class="mt-4 px-6 py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700">BANNER BUTTON</button>
+  </div>
+  <div class="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
+    <div class="w-3 h-3 bg-white rounded-full"></div>
+    <div class="w-3 h-3 bg-gray-500 rounded-full"></div>
+    <div class="w-3 h-3 bg-gray-500 rounded-full"></div>
+  </div>
+</div>
+
         </div>
       </section>
-      <button @click="qoshish">BOSSSSSSSSSSSSSSSSSSSSSSSS</button>
+      <!-- <button @click="qoshish">BOSSSSSSSSSSSSSSSSSSSSSSSS</button> -->
     </main>
   </div>
 </template>
